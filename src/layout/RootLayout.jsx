781ff70f981160c1804744bd91237af5b0e7de1c
choikyungsoo <ryan1954@naver.com>
2025-07-31 20:57:21 +0900

    Design: 화면 최소 사이즈 설정

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -3,9 +3,11 @@ import {Outlet} from "react-router-dom";
 
 const RootLayout = () => {
   return (
-    <>
-      <Outlet/>
-    </>
+    <div className="flex justify-center w-dvw h-dvh">
+      <div className="min-w-(--min-screen-size) h-full">
+        <Outlet/>
+      </div>
+    </div>
   );
 };
 
