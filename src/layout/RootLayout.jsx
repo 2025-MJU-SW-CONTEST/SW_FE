73fd63e77c14ca5f929a584175032c48de923232
choikyungsoo <ryan1954@naver.com>
2025-08-08 19:54:44 +0900

    Feat: 프로텍트 라우터 임시 설정

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import {Outlet} from "react-router-dom";
+import {Navigate, Outlet} from "react-router-dom";
+import {useState} from "react";
+import {pagePath} from "@routes/pagePath.js";
 
 const RootLayout = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
+  if(!isLogin){
+    return (
+      <Navigate to={'/' + pagePath.ONBOARDING} replace/>
+    )
+  }
   return (
     <div className="flex justify-center w-dvw h-dvh">
       <div className="w-(--min-screen-size) h-full">
--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Onboarding = () => {
   return (
     <div>
-      
+      온보딩페이지 입니다. 
     </div>
   );
 };
