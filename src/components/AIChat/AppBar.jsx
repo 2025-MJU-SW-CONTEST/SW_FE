1a386e58eb8c9766d6161780c18b5acb067c465d
choikyungsoo <ryan1954@naver.com>
2025-08-12 16:40:54 +0900

    Design: AppBar 글자 크기 수정

--- src/components/AIChat/AppBar.jsx
+++ src/components/AIChat/AppBar.jsx
@@ -6,7 +6,7 @@ const AppBar = () => {
   return (
     <div className="flex items-end gap-013 h-13.5 pb-010 pl-[19px] pt-010 rounded-bl-010 rounded-br-010 shadow-[0px_1px_9.699999809265137px_0px_rgba(227,227,227,1.00)]">
       <div className="w-[31px] h-[31px] rounded-2xl bg-secondary-50"/>
-      <p className="pretendard_bold">{t("title_fily")}</p>
+      <p className="pretendard_bold text-020">{t("title_fily")}</p>
     </div>
   );
 };
