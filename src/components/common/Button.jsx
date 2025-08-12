1ba9c52b49c7ae5121be4f62f91848ccda577208
choikyungsoo <ryan1954@naver.com>
2025-08-12 16:21:34 +0900

    Design: 값입력시 글자수 표시 및 경고문구 표시 추가

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,19 +1,19 @@
 import clsx from "clsx";
 
-const Button = ({ text, type, className, onClick }) => {
+const Button = ({ text, type, className, onClick, isActive }) => {
   const buttonClass = clsx(
     "font-family-pretendard w-full h-[50px] rounded-006 cursor-pointer leading-loose text-016 font-bold focus:outline-none",
-    {
+    isActive ? {
       "bg-primary text-primary-50": type === "normal",
-      "bg-gradient-to-r from-primary to-secondary-500 text-primary-50":
-        type === "emphasis",
+      "bg-gradient-to-r from-primary to-secondary-500 text-primary-50": type === "emphasis",
       "bg-font-50 text-font-400": type === "disabled",
-    },
+    } : "bg-font-200 text-font-400"
+    ,
     className
   );
 
   return (
-    <button className={buttonClass} onClick={onClick}>
+    <button className={buttonClass} onClick={onClick} disabled={!isActive}>
       {text}
     </button>
   );
