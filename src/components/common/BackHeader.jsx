207c0e73eefaa38a110585012e0ed2df3ef6634c
rhdmschdl <lovelyskygir@naver.com>
2025-08-13 22:57:25 +0900

    Design: 윗쪽 마진 조정

--- src/components/common/BackHeader.jsx
+++ src/components/common/BackHeader.jsx
@@ -2,7 +2,7 @@ import BackButton from "@/components/common/BackButton";
 
 const BackHeader = ({ label, onBack }) => {
   return (
-    <header className="flex items-center px-7.5 pt-[59px] rounded-b-010 pb-4.5 shadow-md shadow-bottom-neutral-200 ">
+    <header className="flex items-center px-7.5 pt-4.5 rounded-b-010 pb-4.5 shadow-md shadow-bottom-neutral-200 ">
       <BackButton variant="icon" onClick={onBack} className="mr-1" />
       <div className="pretendard_bold text-020 leading-7.5">{label}</div>
     </header>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,7 @@ import KakaoLoginButton from "@/components/login/KakaoLoginButton";
 const Login = () => {
   return (
     <div className="flex flex-col items-center gap-[253.21px]">
-      <div className="mt-[301px]">
+      <div className="mt-63.5">
         <img src="img/login.png" />
       </div>
       <div className="mb-[89px]">
