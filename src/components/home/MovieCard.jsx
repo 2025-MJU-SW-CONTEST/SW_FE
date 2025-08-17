1eb4b51bc8442b281490bccbd4c467e5bd879765
rhdmschdl <lovelyskygir@naver.com>
2025-08-18 00:37:12 +0900

    Feat: IntersectionObserver로 보일 때 src를 주입하는 커스텀 레이지 로딩

--- src/components/home/MovieCard.jsx
+++ src/components/home/MovieCard.jsx
@@ -1,16 +1,26 @@
 import MoviePoster from "./MoviePoster";
 import MovieTitle from "./MovieTitle";
 import MovieRating from "./MovieRating";
+import { toImageUrl } from "@/utils/images";
 
-const MovieCard = ({ movie, onClick }) => {
+const MovieCard = ({ movie, onClick, eager = false }) => {
   return (
     <div onClick={onClick} className="cursor-pointer">
-      <div className="w-80 h-50 rounded-010 bg-secondary-50 shadow-custom-drop hover:shadow-lg transition-shadow overflow-hidden">
-        <MoviePoster src={movie.posterUrl} alt={movie.title} />
+      <div className="w-80 h-50 bg-secondary-50 shadow-custom-drop hover:shadow-lg transition-shadow overflow-hidden">
+        <MoviePoster
+          src={toImageUrl(movie.thumbnailUrl)}
+          alt={movie.title}
+          eager={eager} // 초기 10개는 true
+          ioRootId="scrollableDiv"
+        />
       </div>
       <div className="flex items-center mt-2.5 px-0.5 justify-between">
-        <MovieTitle title={movie.title} />
-        <MovieRating rating={movie.rating} />
+        <div className="flex-1 min-w-0 max-w-43">
+          <MovieTitle title={movie.title} />
+        </div>
+        <div className="shrink-0">
+          <MovieRating rating={movie.rating} />
+        </div>
       </div>
     </div>
   );
