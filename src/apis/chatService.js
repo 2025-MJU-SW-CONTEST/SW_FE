0f78006e99b422dd902883f423e30805675dab04
choikyungsoo <ryan1954@naver.com>
2025-08-18 10:23:01 +0900

    Chore: api용 작성

---
+++ src/apis/chatService.js
@@ -0,0 +1,19 @@
+import {axiosInstance} from "@apis/axiosInstance.js";
+
+export const chatService = {
+  postNewAIChatRooms: async () => {
+    const res = await axiosInstance.post(
+      '/api/aiChatRooms/new'
+    )
+    return res.data
+  },
+  getAIChatRoomsHistory: async ({id, page, size}) => {
+    const res = await axiosInstance.get(
+      `/api/aiChatRooms/${id}/history`,
+    {
+        params: {page, size}
+      }
+    )
+    return res.data
+  }
+}
